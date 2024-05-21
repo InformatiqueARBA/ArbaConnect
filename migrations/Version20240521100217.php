a9646f035b7408e4b89f955c9f9eb9727c42a981
KevinARBA <kchartier@arba.coop>
2024-05-21 12:12:47 +0200

    création du fichier de migration (pour prendre en compte les mofications ) et migration en BBD

---
+++ migrations/Version20240521100217.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240521100217 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE order_detail CHANGE item_number item_number VARCHAR(40) NOT NULL, CHANGE oder_delivery_date ora_delivery_date DATE DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE order_detail CHANGE item_number item_number INT NOT NULL, CHANGE ora_delivery_date oder_delivery_date DATE DEFAULT NULL');
+    }
+}
